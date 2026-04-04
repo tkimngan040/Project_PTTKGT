e1ba16ea8fba2c318939c8810050c40798069b7d
tritm0427-eng <tritm0427@ut.edu.vn>
2026-04-04 15:32:44 +0700

    Update evaluation.cpp

--- source_code/evaluation.cpp
+++ source_code/evaluation.cpp
@@ -1,5 +1,7 @@
 #include "models.h"
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -26,34 +28,35 @@ string evaluateRoutes(const RouteResult& shortest, const RouteResult& traffic) {
     if (costA > 0)
         costDiffPercent = ((costA - costB) / costA) * 100.0;
 
+    // ===== FORMAT OUTPUT =====
+    stringstream ss;
+    ss << fixed << setprecision(2);
+
+    ss << "So sanh hai tuyen:\n";
+
+    ss << "- Tuyen ngan nhat co distance = " << distA;
+    ss << ", traffic cost = " << costA << "\n";
+
+    ss << "- Tuyen chi phi giao thong thap nhat co distance = " << distB;
+    ss << ", traffic cost = " << costB << "\n";
+
+    ss << "\nChenh lech:\n";
+    ss << "- Tang khoang cach: " << distDiffPercent << " %\n";
+    ss << "- Giam chi phi giao thong: " << costDiffPercent << " %\n\n";
+
     // ===== DANH GIA =====
-    string result = "";
-
-    result += "So sanh hai tuyen:\n";
-
-    result += "- Tuyen ngan nhat co distance = " + to_string(distA);
-    result += ", traffic cost = " + to_string(costA) + "\n";
-
-    result += "- Tuyen chi phi giao thong thap nhat co distance = " + to_string(distB);
-    result += ", traffic cost = " + to_string(costB) + "\n";
-
-    result += "\nChenh lech:\n";
-    result += "- Tang khoang cach: " + to_string(distDiffPercent) + " %\n";
-    result += "- Giam chi phi giao thong: " + to_string(costDiffPercent) + " %\n\n";
-
-    // ===== LOGIC =====
     if (costDiffPercent > 20 && distDiffPercent < 15) {
-        result += "Ket luan: Nen chon tuyen chi phi giao thong thap nhat.\n";
-        result += "Ly do: Giam duoc nhieu ap luc giao thong trong khi chi tang nhe khoang cach.";
+        ss << "Ket luan: Nen chon tuyen chi phi giao thong thap nhat.\n";
+        ss << "Ly do: Giam duoc nhieu ap luc giao thong trong khi chi tang nhe khoang cach.";
     }
     else if (costDiffPercent < 10 && distDiffPercent > 20) {
-        result += "Ket luan: Nen chon tuyen ngan nhat.\n";
-        result += "Ly do: Loi ich giam giao thong khong dang ke nhung phai di vong xa.";
+        ss << "Ket luan: Nen chon tuyen ngan nhat.\n";
+        ss << "Ly do: Loi ich giam giao thong khong dang ke nhung phai di vong xa.";
     }
     else {
-        result += "Ket luan: Hai tuyen gan nhu tuong duong.\n";
-        result += "Ly do: Chenh lech giua khoang cach va giao thong khong dang ke.";
+        ss << "Ket luan: Hai tuyen gan nhu tuong duong.\n";
+        ss << "Ly do: Chenh lech giua khoang cach va giao thong khong dang ke.";
     }
 
-    return result;
+    return ss.str();
 }
